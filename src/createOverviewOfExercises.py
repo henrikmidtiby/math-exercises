d00d6774dbf2c7964ebb64c7e6eb6829632d4201
Henrik Skov Midtiby <hemi@mmmi.sdu.dk>
2017-03-13 10:37:32 +0100

    Massiv simplificering af kode pga. glob modulet.

--- src/createOverviewOfExercises.py
+++ src/createOverviewOfExercises.py
@@ -10,10 +10,7 @@
 # Date: 2014-01-15
 
 # Load dependencies
-import sys
-import os
 import re
-import os
 import glob
 
 
@@ -25,61 +22,27 @@ def count_number_of_exercises_in_file(filename):
             res = pattern_exercise_begin.match(line)
             if res:
                 number_of_exercises += 1
-                print("exercise found: %s" % line[:-1])
 
     return number_of_exercises
 
 
-# main function that iterates through all subdirs and analyzes all files in
-# each subdir.
-# Extracted comments are stored in the file "statistics".
 def main():
-    pattern_latex_file = re.compile(".*tex$")
     output_file = open('exerciseOverview.txt', 'w')
 
-    for filename in glob.iglob('**/*.tex', recursive=True):
+    latex_files = list(glob.iglob('**/*.tex', recursive=True))
+    latex_files.sort()
+    for filename in latex_files:
         print(filename)
-
-    sub_directories = get_list_of_subdirectories('./')
-    for subdir in sub_directories:
-        print("Subdir: %s" % subdir)
-        assignment = subdir[2:]
-        output_file.write(assignment + "\n")
-        temp_dir = subdir + '/'
-        files = get_files_in_directory(temp_dir)
-        for filename in files:
-            if pattern_latex_file.match(filename):
-                full_filename = assignment + "/" + filename
-                number_of_exercises = count_number_of_exercises_in_file(full_filename)
-                try:
-                    output_file.write("\t%s - %d\n" % (filename, number_of_exercises))
-                except Exception as E:
-                    print("Error analyzing file: %s" % full_filename)
-                    print(E)
+        full_filename = filename
+        number_of_exercises = count_number_of_exercises_in_file(full_filename)
+        try:
+            output_file.write("\t%s - %d\n" % (filename, number_of_exercises))
+        except Exception as E:
+            print(E)
 
     output_file.close()
 
     pass
-
-
-def get_list_of_subdirectories(path):
-    dirs = []
-    files_in_dir = os.listdir(path)
-    for file_in_dir in files_in_dir:
-        if os.path.isdir(path + file_in_dir):
-            dirs.append(path + file_in_dir)
-    dirs.sort()
-    return dirs
-
-
-def get_files_in_directory(path):
-    files = []
-    files_in_dir = os.listdir(path)
-    for file_in_dir in files_in_dir:
-        if os.path.isfile(path + file_in_dir):
-            files.append(file_in_dir)
-    files.sort()
-    return files
 
 
 main()
