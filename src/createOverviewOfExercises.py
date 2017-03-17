9f7be578bcff29d17e42eaf601b0eb1cd7b5e779
Henrik Skov Midtiby <hemi@mmmi.sdu.dk>
2017-03-17 11:03:46 +0100

    Skab overblik over de eksisterende opgaver, inkl antal delopgaver i de enkelte undertræer.

--- src/createOverviewOfExercises.py
+++ src/createOverviewOfExercises.py
@@ -12,6 +12,7 @@
 # Load dependencies
 import re
 import glob
+from collections import defaultdict
 
 
 def count_number_of_exercises_in_file(filename):
@@ -26,20 +27,55 @@ def count_number_of_exercises_in_file(filename):
     return number_of_exercises
 
 
+def get_all_sub_paths(filename):
+    parts_of_filename = filename.split("/")
+    for k in range(len(parts_of_filename) + 1):
+        yield parts_of_filename[0:k]
+
+
+def is_new_path_included_in_the_reference_path(reference_path, new_path):
+    if len(reference_path) < len(new_path):
+        return False
+    for k in range(len(new_path)):
+        if reference_path[k] != new_path[k]:
+            return False
+    return True
+
+
+def leaf_of_path_prefixed_with_tabs(path, text):
+    number_of_indents = len(path) - 1
+    return "\t"*number_of_indents + path[-1] + text
+
+
+def generated_tree_expression(list_of_filenames, number_of_exercises_in_files):
+    output = ""
+    last_path = []
+    for filename in list_of_filenames:
+        parts_of_filename = filename.split("/")
+        for val in get_all_sub_paths(filename):
+            if not is_new_path_included_in_the_reference_path(last_path, val):
+                number_of_exercises = number_of_exercises_in_files["/".join(val)]
+                text = " (%d)" % number_of_exercises
+                output += leaf_of_path_prefixed_with_tabs(val, text) + "\n"
+        last_path = parts_of_filename
+    return output
+
+
 def main():
     output_file = open('exerciseOverview.txt', 'w')
 
     latex_files = list(glob.iglob('**/*.tex', recursive=True))
     latex_files.sort()
+    number_of_exercises_in_files = defaultdict(int)
     for filename in latex_files:
-        print(filename)
         full_filename = filename
         number_of_exercises = count_number_of_exercises_in_file(full_filename)
-        try:
-            output_file.write("\t%s - %d\n" % (filename, number_of_exercises))
-        except Exception as E:
-            print(E)
+        for sub_path in get_all_sub_paths(filename):
+            number_of_exercises_in_files["/".join(sub_path)] += number_of_exercises
 
+    exercise_tree = generated_tree_expression(latex_files, number_of_exercises_in_files)
+    print(exercise_tree)
+    output_file.write(exercise_tree)
     output_file.close()
 
     pass
