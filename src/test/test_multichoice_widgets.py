fe312c1c1bf68ddc932b1f06e02363c92c769da7
Henrik Skov Midtiby <hemi@mmmi.sdu.dk>
2017-05-31 13:33:17 +0200

    Added more unittests.

---
+++ src/test/test_multichoice_widgets.py
@@ -0,0 +1,34 @@
+from ..ChangeMultiChoiceMarkup import *
+from ..WidgetRenderer import *
+from nose.tools import assert_equals
+
+
+def test_render_sorting_widget_1():
+    input_values = [MultiChoice(nr=1,
+                                items=[MultiChoiceItem(text='ja', is_correct_answer='true'),
+                                       MultiChoiceItem(text='nej', is_correct_answer='false')],
+                                parameters=['randomizeorder', 'selectmultiple'])]
+    expected_output = """
+    "multi_choice1": {
+        "type": "multiple-choice",
+        "properties": {
+            "choices": [{
+                "title": "ja",
+                "correct": true
+                },{
+                "title": "nej",
+                "correct": false
+                }]
+        },
+        "name": "multi_choice1"
+    }
+    """
+
+    wr = WidgetRenderer()
+    wr.add_multi_choice_widgets(input_values)
+    actual_output = wr.get_rendered_widgets()
+
+    print(expected_output)
+    print(actual_output[0].content)
+    assert_equals(expected_output, actual_output[0].content)
+
