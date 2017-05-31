b6ae3e816ddc7d942e7ab4b73e9749ccb62d2bae
Henrik Skov Midtiby <hemi@mmmi.sdu.dk>
2017-05-31 13:36:18 +0200

    Added more unittests.

--- src/test/test_create_overview_of_exercises.py
+++ src/test/test_create_overview_of_exercises.py
@@ -6,3 +6,27 @@ def test_get_all_sub_paths():
     output = list(get_all_sub_paths(input))
     expected_output = [[], ["a"], ["a", "b"], ["a", "b", "c"]]
     assert expected_output == output
+
+
+def test_is_new_path_included_in_the_reference_path_1():
+    input_a = ["a"]
+    input_b = ["b"]
+    assert is_new_path_included_in_the_reference_path(input_a, input_b) == False
+
+
+def test_is_new_path_included_in_the_reference_path_2():
+    input_a = ["a"]
+    input_b = ["a"]
+    assert is_new_path_included_in_the_reference_path(input_a, input_b) == True
+
+
+def test_is_new_path_included_in_the_reference_path_3():
+    input_a = ["a", "b"]
+    input_b = ["a"]
+    assert is_new_path_included_in_the_reference_path(input_a, input_b) == True
+
+
+def test_is_new_path_included_in_the_reference_path_4():
+    input_a = ["a", "b"]
+    input_b = ["b"]
+    assert is_new_path_included_in_the_reference_path(input_a, input_b) == False
