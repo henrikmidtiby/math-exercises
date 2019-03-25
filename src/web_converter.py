863dd7be7b0c84a403a8f3e4ce2666269ee54cfc
Henrik Skov Midtiby <hemi@mmmi.sdu.dk>
2019-03-25 21:50:30 +0100

    Improvements to the web converter.

--- src/web_converter.py
+++ src/web_converter.py
@@ -1,12 +1,14 @@
 # from app import app
-from helper.exerciseconverterfunctions import get_exercises, render_exercises
+from .exercise_converter.helper.exerciseconverterfunctions import get_exercises, render_exercises, change_part_of_markup
 
 from flask import Flask, request, redirect, url_for, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return render_template('index.html', converted_exercise="")
+    return render_template('index.html', 
+            converted_exercise="",
+            original_input="")
 
 @app.route('/name/<name>')
 def hello_world_named(name):
@@ -16,10 +18,16 @@ def hello_world_named(name):
 def submit():
     if request.method == 'POST':
         converted_exercise = request.form['exercise_input']
-        exercises = list(get_exercises(change_part_of_markup(converted_exercise)))
+        rendered_exercises = converted_exercise
+        exercises = list(get_exercises(change_part_of_markup(converted_exercise.split('\n'))))
         rendered_exercises = render_exercises(exercises)
+        print(str(rendered_exercises))
+        my_bytes = str(rendered_exercises).encode("utf-8")
+        simplified = my_bytes.decode("unicode-escape")
+        print(simplified)
         return render_template('index.html',
-                converted_exercise=rendered_exercises)
+                converted_exercise=simplified, 
+                original_input=converted_exercise)
     else:
         return render_template('index.html')
         
